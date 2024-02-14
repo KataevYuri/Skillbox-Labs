9dc47887e5ba571378e3771ceaa41efb7e2712cc
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2024-02-14 18:25:37 +0800

    new project

---
+++ 18_Media_Maps_Webview/lab18_camera/README.md
@@ -0,0 +1,3 @@
+# lab18_camera
+
+A new Flutter project.
---
+++ 18_Media_Maps_Webview/lab18_camera/android/app/src/main/kotlin/com/example/lab18_camera/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lab18_camera
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
