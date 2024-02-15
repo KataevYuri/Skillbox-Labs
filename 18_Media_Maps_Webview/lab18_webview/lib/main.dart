0adb6aad7fd8b6fad4c182cde207bf812ef95c62
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2024-02-16 00:06:38 +0800

    18.4 complete

--- 18_Media_Maps_Webview/lab18_webview/lib/main.dart
+++ 18_Media_Maps_Webview/lab18_webview/lib/main.dart
@@ -23,50 +23,84 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  InAppWebViewController? _webViewController;
+  InAppWebViewSettings? _settings;
+  TextEditingController? _urlText;
+
   @override
   void initState() {
     super.initState();
+    _urlText = TextEditingController(text: 'https://github.com');
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Lab #18.4 - Webview'),
-        ),
-        body: SizedBox(
-          height: 400,
-          width: double.infinity,
-          child: Column(children: [
-            Row(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light(),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Lab #18.4 - Webview'),
+            ),
+            body: Column(
               children: [
+                Row(
+                  children: [
+                    IconButton(
+                        onPressed: () async {
+                          if (_webViewController != null) {
+                            if (await _webViewController!.canGoBack()) {
+                              _webViewController!.goBack();
+                            }
+                          }
+                        },
+                        icon: const Icon(Icons.arrow_back)),
+                    IconButton(
+                        onPressed: () async {
+                          if (_webViewController != null) {
+                            if (await _webViewController!.canGoForward()) {
+                              _webViewController!.goForward();
+                            }
+                          }
+                        },
+                        icon: const Icon(Icons.arrow_forward)),
+                    IconButton(
+                        onPressed: () => {
+                              _webViewController!.loadUrl(
+                                  urlRequest: URLRequest(
+                                url: WebUri(_urlText!.text),
+                              ))
+                            },
+                        icon: const Icon(Icons.refresh)),
+                    Expanded(
+                      child: TextFormField(
+                        controller: _urlText,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: 'Enter url',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
                 Expanded(
-                    child: ElevatedButton(
-                        onPressed: () => {}, child: const Text('-10'))),
-                Expanded(
-                    child: ElevatedButton(
-                        onPressed: () => {}, child: const Text('+10')))
+                  child: InAppWebView(
+                    onWebViewCreated: (controller) {
+                      _webViewController = controller;
+                    },
+                    initialSettings: _settings,
+                    initialUrlRequest: URLRequest(
+                      url: WebUri('https://skillbox.ru'),
+                    ),
+                  ),
+                )
               ],
-            )
-          ]),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {});
-          },
-          child: const Icon(
-            Icons.play_arrow,
-          ),
-        ),
-      ),
-    );
+            )));
   }
 
   @override
   void dispose() {
+    _webViewController?.dispose();
     super.dispose();
   }
 }
