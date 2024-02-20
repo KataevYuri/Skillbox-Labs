fde4b6cca23ad5fb6bffe37e8a4bdca1124c5103
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2024-02-20 11:10:40 +0800

    1

--- 19_Integration/flutter_integrations/lib/main.dart
+++ 19_Integration/flutter_integrations/lib/main.dart
@@ -9,9 +9,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData.dark(),
       home: Scaffold(
-        body: Center(
+        appBar: AppBar(
+          title: const Text('Lab #19. Integration'),
+        ),
+        body: const Center(
           child: Text('Hello World!'),
         ),
       ),
