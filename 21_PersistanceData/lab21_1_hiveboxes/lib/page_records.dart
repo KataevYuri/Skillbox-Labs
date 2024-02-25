d726b7476b7fb21d6a8f3d1177387b628aa861d0
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2024-02-25 18:30:49 +0800

    Update page_records.dart

--- 21_PersistanceData/lab21_1_hiveboxes/lib/page_records.dart
+++ 21_PersistanceData/lab21_1_hiveboxes/lib/page_records.dart
@@ -10,13 +10,15 @@ class RecordsPage extends StatefulWidget {
 }
 
 class _RecordsPageState extends State<RecordsPage> {
-  Box<Categories>? _recName;
-
+  Categories? _selectedCat;
+  Box<Categories>? _categoriesBox;
+  final Box<Categories> _box = Hive.box('database1');
   @override
   void didChangeDependencies() {
     final args = ModalRoute.of(context)?.settings.arguments;
 
-    _recName = args as Box<Categories>;
+    _selectedCat = args as Categories;
+    _categoriesBox = _box.get<Categories>(_selectedCat!);
     setState(() {});
     super.didChangeDependencies();
   }
@@ -25,13 +27,34 @@ class _RecordsPageState extends State<RecordsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(_recName!.name),
+        title: Text(_selectedCat!.name),
       ),
       body: const SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Center(child: Text('Records Page')),
+            Expanded(
+              child: (n == null)
+                  ? n
+                  : ValueListenableBuilder(
+                      valueListenable: _categoriesBox!.listenable(),
+                      builder: (context, Box<Categories> box, widget) {
+                        return ListView.builder(
+                            itemCount: box.length,
+                            itemBuilder: (_, index) {
+                              final item = box.values.elementAt(index);
+                              return Card(
+                                child: ListTile(
+                                  title: Text(item.name),
+                                  onTap: () => Navigator.of(context).pushNamed(
+                                    '/records',
+                                    arguments: item,
+                                  ),
+                                ),
+                              );
+                            });
+                      }),
+            ),
           ],
         ),
       ),
