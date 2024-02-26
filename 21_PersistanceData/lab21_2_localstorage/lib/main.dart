4bfbbdb6ae3a5ffe6e6c801bb6815f68bd6573f6
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2024-02-26 23:37:22 +0800

    lab2 complete

--- 21_PersistanceData/lab21_2_localstorage/lib/main.dart
+++ 21_PersistanceData/lab21_2_localstorage/lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
   runApp(const MainApp());
 }
 
@@ -19,13 +21,14 @@ class _MainAppState extends State<MainApp> {
   late TextEditingController _file;
   List<String> images = [];
   late String localPath;
+  late File file;
 
   @override
   void initState() {
     super.initState();
     _file = TextEditingController();
     _file.text = 'https://free-images.com/md/4275/penguin_funny_blue_water.jpg';
-    //initialize();
+    initialize();
   }
 
   @override
@@ -38,6 +41,7 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
+        useMaterial3: true,
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
@@ -50,31 +54,38 @@ class _MainAppState extends State<MainApp> {
         body: Center(
           child: Column(
             children: [
-              (images.isEmpty)
-                  ? const Text('No Files')
-                  : ListView.separated(
-                      itemCount: images.length,
-                      separatorBuilder: (BuildContext context, int index) =>
-                          const Divider(),
-                      itemBuilder: (BuildContext context, int index) {
-                        return ListTile(
-                          title: Image.file(File(images[index])),
-                          subtitle: Text(images[index]),
-                        );
-                      },
-                    ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 5.0),
-                child: Row(
-                  children: [
-                    TextField(
-                      controller: _file,
-                    ),
-                    IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.download)),
-                  ],
-                ),
-              )
+              Expanded(
+                  child: (images.isEmpty)
+                      ? const Text('No Files')
+                      : ListView.separated(
+                          itemCount: images.length,
+                          separatorBuilder: (BuildContext context, int index) =>
+                              const Divider(),
+                          itemBuilder: (BuildContext context, int index) {
+                            return ListTile(
+                              title: Image.file(File(images[index])),
+                              subtitle: Text(images[index]),
+                            );
+                          },
+                        )),
+              TextField(
+                controller: _file,
+              ),
+              IconButton(
+                  onPressed: () async {
+                    var response = await get(Uri.parse(_file.text));
+                    var firstPath = '$localPath/images';
+                    var filePathAndName =
+                        '$localPath/images/${basename(_file.text)}';
+
+                    await Directory(firstPath).create(recursive: true);
+                    File file2 = File(filePathAndName);
+                    file2.writeAsBytesSync(response.bodyBytes);
+                    setState(() {
+                      images.add(filePathAndName);
+                    });
+                  },
+                  icon: const Icon(Icons.download)),
             ],
           ),
         ),
