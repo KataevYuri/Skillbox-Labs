dee4392585ed801c36d9e1a209a8be3d0ac4e978
KataevYuri <107112301+KataevYuri@users.noreply.github.com>
2024-02-27 02:11:03 +0800

    Update main.dart

--- 22_Flutter Engine/lab22_1_flutter_engine/lib/main.dart
+++ 22_Flutter Engine/lab22_1_flutter_engine/lib/main.dart
@@ -4,17 +4,39 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        appBar: MyAppBar(),
         body: Center(
-          child: Text('Hello World!'),
+          child: Text('Lab #22. Custom App bar'),
         ),
       ),
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({
+    super.key,
+  });
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.amber,
+      centerTitle: true,
+      title: const Text('Lab #22.Title'),
     );
   }
 }
